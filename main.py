0ae2d8819ceb06868b4370d5045b26640029de86
Gabija G <taca3loban@gmail.com>
2022-09-14 16:30:25 +0300

    su Python ciklu

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template
+
+# app = Flask(__name__)
+#
+# @app.route("/<vardas>")
+# def home(vardas):
+#     return f"Labas, {vardas}"
+#
+# if __name__ == "__main__":
+#     app.run(debug=True)
+
+# ats http://127.0.0.1:5000/Gabija, vietoje vardo belen kas gali buti
 
 app = Flask(__name__)
 
-@app.route("/<vardas>")
-def home(vardas):
-    return f"Labas, {vardas}"
+@app.route("/")
+def home():
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
-
-# ats http://127.0.0.1:5000/Gabija, vietoje vardo belen kas gali buti
