617740e374ba91a74347c83d46011aa1ecfc3e44
Gabija G <taca3loban@gmail.com>
2022-09-14 15:58:58 +0300

    mano pirmas Flask puslapis

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return "<h1>Cia mano pirmas flask puslapis</h>"
+@app.route("/<vardas>")
+def home(vardas):
+    return f"Labas, {vardas}"
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+# ats http://127.0.0.1:5000/Gabija, vietoje vardo belen kas gali buti
